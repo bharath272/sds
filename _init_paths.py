97c050a6adb5543b77995d15b947b951d65f20ef
Bharath Hariharan <bharath2@eecs.berkeley.edu>
2015-08-23 18:43:05 -0400

    change paths

--- _init_paths.py
+++ _init_paths.py
@@ -3,7 +3,7 @@ import sys
 this_dir = osp.dirname(__file__)
 
 # Add caffe to PYTHONPATH
-caffe_path = osp.join(this_dir,'caffe', 'python')
+caffe_path = osp.join(this_dir,'caffe-sds', 'python')
 sys.path.insert(0,caffe_path)
 
 
