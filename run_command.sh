3ae2974a44efa1a182dc5d0348aba2d0d2e89270
Bharath Hariharan <bharath2@eecs.berkeley.edu>
2015-09-08 14:14:10 -0400

    download training data

--- run_command.sh
+++ run_command.sh
@@ -1,1 +1,5 @@
-PYTHONUNBUFFERED="True" PYTHONPATH=:caffe/python/ GLOG_logtostderr=1 caffe/build/tools/caffe.bin train   -gpu 2   -solver model_defs/solver.prototxt -weights /work5/bharath2/vgg16_fast_rcnn_MCGdedup5K_voc_2012_train_iter_40000.caffemodel 
+#download training and gt and initial model
+wget http://www.eecs.berkeley.edu/Research/Projects/CS/vision/shape/hypercolumn/train_mcg_boxes.pkl
+wget http://www.eecs.berkeley.edu/Research/Projects/CS/vision/shape/hypercolumn/gttrain.pkl
+wget http://www.eecs.berkeley.edu/Research/Projects/CS/vision/shape/hypercolumn/vgg16_fast_rcnn_MCGdedup5K_voc_2012_train_iter_40000.caffemodel
+PYTHONUNBUFFERED="True" PYTHONPATH=:caffe/python/ GLOG_logtostderr=1 caffe/build/tools/caffe.bin train   -gpu 2   -solver model_defs/solver.prototxt -weights vgg16_fast_rcnn_MCGdedup5K_voc_2012_train_iter_40000.caffemodel 
